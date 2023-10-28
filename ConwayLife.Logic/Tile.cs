8e1797b057a39e4b7faad877f4df5e58ff4da6db
MightyBeast <zgpoter@gmail.com>
2023-10-28 15:26:00 +0300

    -feat- : Added indexes fot tile.

--- ConwayLife.Logic/Tile.cs
+++ ConwayLife.Logic/Tile.cs
@@ -2,6 +2,14 @@ namespace ConwayLife.Logic;
 
 public class Tile
 {
+    public readonly int i;
+    public readonly int j;
     public bool alive;
     public Tile[]? neighbours;
+
+    public Tile(int i, int j)
+    {
+        this.i = i;
+        this.j = j;
+    }
 }
--- ConwayLife.Tests/TileTests.cs
+++ ConwayLife.Tests/TileTests.cs
@@ -9,8 +9,10 @@ public class TileTests
     [Test]
     public void TileInitIsCorrect()
     {
-        Tile tile = new();
+        Tile tile = new(1, 2);
 
+        tile.i.Should().Be(1);
+        tile.j.Should().Be(2);
         tile.alive.Should().BeFalse();
         tile.neighbours.Should().BeNullOrEmpty();
     }
